0ef210b8c36be85c34880c6caa5d27f5637590dc
matriz <cyruschan0111@gmail.com>
2020-03-21 16:24:11 +0800

    0.2.2 kccsofficial
    bug fixes,
    color mode fixes
    clear code junks
    clear screen before run
    performance increase
    future versions will be fused into the new discord bot.

--- kccsofficial.py
+++ kccsofficial.py
@@ -2,6 +2,17 @@ from mcstatus import MinecraftServer
 import sys, os
 from termcolor import colored, cprint
 
+def clearscreen(): 
+    # for windows 
+    if os.name == 'nt': 
+        _ = os.system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = os.system('clear') 
+clearscreen()
+print("\033[34m=============================KCCS OFFICIAL=============================\033[0m")
+
+
 # user-defined exceptions
 class Error(Exception):
    """Base class for other exceptions"""
@@ -20,16 +31,16 @@ if sys.platform.lower() == "win32":
 
 # Group of Different functions for different styles
 class style():
-    black      = lambda x="": '\033[30m' + str(x)
-    red        = lambda x="": '\033[31m' + str(x)
-    green      = lambda x="": '\033[32m' + str(x)
-    yellow     = lambda x="": '\033[33m' + str(x)
-    blue       = lambda x="": '\033[34m' + str(x)
-    magenta    = lambda x="": '\033[35m' + str(x)
-    cyan       = lambda x="": '\033[36m' + str(x)
-    white      = lambda x="": '\033[37m' + str(x)
-    underline  = lambda x="": '\033[4m' + str(x)
-    reset      = lambda x="": '\033[0m' + str(x)
+    black      = lambda x="": '\33[30m' + str(x)
+    red        = lambda x="": '\33[31m' + str(x)
+    green      = lambda x="": '\33[32m' + str(x)
+    yellow     = lambda x="": '\33[33m' + str(x)
+    blue       = lambda x="": '\33[34m' + str(x)
+    magenta    = lambda x="": '\33[35m' + str(x)
+    cyan       = lambda x="": '\33[36m' + str(x)
+    white      = lambda x="": '\33[37m' + str(x)
+    underline  = lambda x="": '\33[4m' + str(x)
+    reset      = lambda x="": '\33[0m' + str(x)
     end        = lambda x="": '\33[0m' + str(x)
     bold       = lambda x="": '\33[1m' + str(x)
     italic     = lambda x="": '\33[3m' + str(x)
@@ -84,155 +95,172 @@ def help():
 def chkmcsrvstatus():
     global hlp
     try:
-        temp = MinecraftServer.lookup("kccscomm.minehut.gg:25565").status()
+        temp = MinecraftServer.lookup("kccscomm.minehut.gg:25565")
         statuskccscomm = "online"
-        playerskccscomm = temp.players.online
     except OSError:
         statuskccscomm = "offline"
-        playerskccscomm = 0
     try:
-        temp = MinecraftServer.lookup("lopxl.aternos.me").status()
+        temp = MinecraftServer.lookup("lopxl.aternos.me")
         statuslopxl = "online"
-        playerslopxl = temp.players.online
     except OSError:
         statuslopxl = "offline"
-        playerslopxl = 0
     try:
-        temp = MinecraftServer.lookup("lopx.minehut.gg").status()
+        temp = MinecraftServer.lookup("lopx.minehut.gg")
         statuslopx = "online"
-        playerslopx = temp.players.online
     except OSError:
         statuslopx = "offline"
-        playerslopx = 0
     try:
-        temp = MinecraftServer.lookup("116kccs.aternos.me").status()
+        temp = MinecraftServer.lookup("116kccs.aternos.me")
         status116 = "online"
-        players116 = temp.players.online
     except OSError:
         status116 = "offline"
-        players116 = 0
-    try: 
-        temp = MinecraftServer.lookup("sb4kccs.aternos.me").status()
+    try:
+        temp = MinecraftServer.lookup("sb4kccs.aternos.me")
         statussb4 = "online"
-        playerssb4 = temp.players.online
     except OSError:
         statussb4 = "offline"
-        playerssb4 = 0
     try:
-        temp = MinecraftServer.lookup("smpkccs.aternos.me").status()
+        temp = MinecraftServer.lookup("smpkccs.aternos.me")
         statussmp = "online"
-        playerssmp = temp.players.online
     except OSError:
         statussmp = "offline"
-        playerssmp = 0
     return {"statuskccscomm":statuskccscomm,
-            "playerskccscomm": playerskccscomm,
             "statuslopxl": statuslopxl,
-            "playerslopxl": playerslopxl,
             "statuslopx":statuslopx,
-            "playerslopx": playerslopx,
             "status116": status116,
-            "players116": players116,
             "statussb4": statussb4,
-            "playerssb4": playerssb4,
-            "statussmp": statussmp,
-            "playerssmp": playerssmp}
+            "statussmp": statussmp}
 
 
 def mc():
     global mcserverarglist  #chk srv arg
+    global tmp
 
     if sys.argv[2] in mcserverarglist:
-        mcsrvstatus = chkmcsrvstatus()
+        try:
+            if sys.argv[3]:
+                if sys.argv[3] == "kccscomm":
+                    link = "kccscomm.minehut.gg"
+                    name = "KCCS Community Survival Server"
+                    note = "Recommened Survival Server"
+                elif sys.argv[3] == "lopx":
+                    link = "lopx.minehut.gg"
+                    name = "Lopixel on Minehut"
+                    note = "Help build the minigames server!"
+                elif sys.argv[3] == "lopxl":
+                    link = "lopxl.aternos.me"
+                    name = "Lopixel on Aternos"
+                    note = "Help build the minigames server!"
+                elif sys.argv[3] == "116kccs":
+                    link = "116kccs.aternos.me"
+                    name = "1.16 Survival Server for KCCS"
+                    note = "Snapshot version "
+                elif sys.argv[3] == "sb4kccs":
+                    link = "sb4kccs.aternos.me"
+                    name = "Skyblock for KCCS"
+                    note = "Ruined :("
+                elif sys.argv[3] == "smpkccs":
+                    link = "smpkccs.aternos.me"
+                    name = "No Cheat Survival Multiplayer"
+                    note = "NO CHEATING"
+                elif sys.argv[3] == "hypixel":
+                    link = "mc.hypixel.net"
+                    name = "Hypixel Network"
+                    note = "Best Minecraft Server Ever!"
+                elif sys.argv[3] == "earthmc":
+                    link = "earthmc.net"
+                    name = "EarthMC"
+                    note = "A towny server"
+                elif sys.argv[3] == "pvpwars":
+                    link = "pvpwars.net"
+                    name = "PvpWars"
+                    try:
+                        note = MinecraftServer(link).query().motd
+                    except OSError as e:
+                        note = str(e)
+                else:
+                    link = sys.argv[3]
+                    name = sys.argv[3].split(".")[0]
+                    try:
+                        note = MinecraftServer(link).query().motd
+                    except OSError as e:
+                        note = str(e)
+                try:
+                    tmp=MinecraftServer(sys.argv[3]).query()
+                    status="online"
+                    players=tmp.players.online
+                    playerls=tmp.players.names
+                except OSError as e:
+                    status="offline"
+                    players=0
+                    playerls=[str(e)]
+                except KeyError as e:
+                    raise InvalidArgument("Invalid server id: " + str(e))
+                try:
+                    ping=str(MinecraftServer(link).ping()) + " ms"
+                except OSError as e:
+                    ping=str(e)
 
+                print((style.yellow("Server name:\t\t")         + style.green("{0}\n")
+                        + style.yellow("Server link:\t\t")      + style.green("{1}\n")
+                        + style.yellow("Server status:\t\t")    + style.green("{2}\n")
+                        + style.yellow("Note:\t\t\t")           + style.green("{3}\n")
+                        + style.yellow("Latency (Ping):\t\t")   + style.green("{4}\n")
+                        + style.yellow("Number of players:\t")  + style.green("{5}\n")
+                        + style.yellow("Players Online:\t\t")   + style.green("{6}\n") + style.reset())
+                    .format(name,                       #format it with the stuff from mcsrvlis and minecraft servers libraries
+                            link,
+                            status,
+                            note,
+                            ping,
+                            players,
+                            ", ".join(playerls)))
+                return 0
+            else:
+                raise InvalidArgument("Invalid argument \"" + sys.argv[3] + "\" for the third argument.")
+        except IndexError:
+            pass
+        mcsrvstatus = chkmcsrvstatus()
         mcsrvlis = {"kccscomm": {
                         "name":             "KCCS Community",
                         "link":             "kccscomm.minehut.gg",
                         "status":           mcsrvstatus["statuskccscomm"],
-                        "playersOnline":    mcsrvstatus["playerskccscomm"],
                         "note":             "Recommened"
                     },
                     "lopxl":{
                         "name":             "Lopixel on Aternos",
                         "link":             "lopxl.aternos.me",
                         "status":           mcsrvstatus["statuslopxl"],
-                        "playersOnline":    mcsrvstatus["playerslopxl"],
                         "note":             "Inactive"
                     },
                     "lopx":{
                         "name":             "Lopixel on Minehut",
                         "link":             "lopx.minehut.gg",
                         "status":           mcsrvstatus["statuslopx"],
-                        "playersOnline":    mcsrvstatus["playerslopx"],
                         "note":             "Ignored"
                     },
                     "116kccs":{
                         "name":             "1.16 server for KCCS",
                         "link":             "116kccs.aternos.me",
                         "status":           mcsrvstatus["status116"],
-                        "playersOnline":    mcsrvstatus["players116"],
                         "note":             "Active"
                     },
                     "sb4kccs":{
                         "name":             "Skyblock for KCCS",
                         "link":             "sb4kccs.aternos.me",
                         "status":           mcsrvstatus["statussb4"],
-                        "playersOnline":    mcsrvstatus["playerssb4"],
                         "note":             "Ruined"
                     },
                     "smpkccs":{
                         "name":             "Survival Multiplayer",
                         "link":             "smpkccs.aternos.me",
                         "status":           mcsrvstatus["statussmp"],
-                        "playersOnline":    mcsrvstatus["playerssmp"],
                         "note":             "Active, NO CHEATING"
                     }
         }
-        try:
-            try:
-                playerls=MinecraftServer(mcsrvlis[sys.argv[3]]["link"]).query().players.names
-            except OSError as e:
-                playerls=[str(e)]
-            except KeyError as e:
-                raise InvalidArgument("Invalid server id: " + str(e))
-            if sys.argv[3] in mcsrvlis:
-                if mcsrvlis[sys.argv[3]]["status"]=="offline":
-                    players="No players!"
-                elif playerls == []:
-                    players="No players!"
-                else:
-                    try:
-                        players=", ".join(MinecraftServer(mcsrvlis[sys.argv[3]]["link"]).query().players.names)
-                    except OSError as e:
-                        players=str(e)
-                try:
-                    ping=str(MinecraftServer(mcsrvlis[sys.argv[3]]["link"]).ping()) + " ms"
-                except OSError as e:
-                    ping=str(e)
-                print((style.yellow("Server name:\t\t")         + style.green("{0}\n") 
-                        + style.yellow("Server link:\t\t")      + style.green("{1}\n") 
-                        + style.yellow("Server status:\t\t")    + style.green("{2}\n") 
-                        + style.yellow("Note:\t\t\t")           + style.green("{3}\n") 
-                        + style.yellow("Latency (Ping):\t\t")   + style.green("{4}\n") 
-                        + style.yellow("Number of players:\t")  + style.green("{5}\n") 
-                        + style.yellow("Players Online:\t\t")   + style.green("{6}\n") + style.reset())
-                    .format(mcsrvlis[sys.argv[3]]["name"],      #format it with the stuff from mcsrvlis and minecraft servers libraries
-                            mcsrvlis[sys.argv[3]]["link"],
-                            mcsrvlis[sys.argv[3]]["status"],
-                            mcsrvlis[sys.argv[3]]["note"],          
-                            ping,
-                            mcsrvlis[sys.argv[3]]["playersOnline"],
-                            players))
-                return 0
-            else:
-                raise InvalidArgument("Invalid argument \"" + sys.argv[3] + "\" for the third argument.")
-        except IndexError:
-            pass
         print(style.black(style.greenbg2("Name                            Link                            Status")) + style.reset() + style.blue2())
         for srv in mcsrvlis:                        #print the stuff in the outer dict
             for key in mcsrvlis[srv]:               #print the value from the key with a for-loop in in the inter dict
-                if key in ["playersOnline","note"]:
-                    continue
                 print(str(mcsrvlis[srv][key]) + "\t\t", end="")
                 if mcsrvlis[srv][key] in ["KCCS Community", "lopx.minehut.gg"]:
                     print("\t",end="")
@@ -247,7 +275,6 @@ def mc():
 
 
 # Main section.  Program officially start from here.
-print(style.blue("\n=============================KCCS OFFICIAL=============================") + style.reset())
 try:
     if sys.argv[1] in mcarglist:                                                          #if user indicate minecraft
         sys.exit(mc())
