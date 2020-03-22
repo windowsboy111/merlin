84c8502268463d1631cdbcc12ccba9dacd747329
matriz <cyruschan0111@gmail.com>
2020-03-22 19:27:10 +0800

    bot 0.6.

---
+++ valstore.py
@@ -0,0 +1,5 @@
+import bot
+loltest = ["!urban MEE6","Am I a joke to you?","!8ball Siriu-smart?","What? Are you a developer?{}".format(ctx.message.author.mention),
+"Didn't expect anyone would use this command, but there it is!","No test.","Ping Pong!","No.","?????","Siriusly, What did you expect?","Stop.","!8ball are you stupid?","Vincidiot"]
+lolcough = ["What? You being infected coronavirus?",str(bot.bot.get_emoji(684291327818596362)),"Please don't:\nSneeze on me;\nCough on me;\nTalk to me,\nNo oh oh!","🤢",
+"Run, run, until it's done, done, until the sun comes up in the morn'."]
