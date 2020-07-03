bbc2254d66783ecef98ab52c33c69be707e7b84f
windowsboy111 <cyruschan0111@gmail.com>
2020-07-03 17:26:05 +0800

    removed excessive files

--- cogs/logcfg.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.config
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='../discordbot.log', encoding='utf-8', mode='a')
-handler.setFormatter(logging.Formatter('[%(asctime)s]%(levelname)s - %(name)s: %(message)s'))
-logger.addHandler(handler)
