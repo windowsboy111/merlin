d8cce70ceb2750c83dfe032c756e3c8357853854
windowsboy111 <cyruschan0111@gmail.com>
2020-07-08 18:28:03 +0800

    added control sql database file stuffs removed vscode settings folder (yes i don't use it)

---
+++ ext/dbctrl.py
@@ -0,0 +1,7 @@
+from contextlib import closing
+import sqlite3
+def close_connection(connection):
+    with closing(connection) as connection:
+        with closing(connection.cursor()) as cursor:
+            rows = cursor.execute("SELECT 1").fetchall()
+            print(rows)
---
+++ ext/init/warnDBinit.py
@@ -0,0 +1,16 @@
+from contextlib import closing
+import sqlite3
+connection = sqlite3.connect("../../samples/warnings.db")
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE warnings (
+    ID int,
+    Person varchar(255),
+    Reason varchar(255),
+    Moderator varchar(255),
+    WarnedDate DATE
+);""")
+
+with closing(connection) as connection:
+    with closing(connection.cursor()) as cursor:
+        rows = cursor.execute("SELECT 1").fetchall()
+        print(rows)
