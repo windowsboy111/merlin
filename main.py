f7c8c8594328a05cb3c4945122a9b40dbdfcb07e
matriz <cyruschan0111@gmail.com>
2020-08-03 04:18:33 +0000

    auto install packages for main.py when fail

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import os
 import sys
 import time
 from dotenv import load_dotenv
-load_dotenv('.env')
+load_dotenv(dotenv_path='.env')
 
 
 def main(port=8080, host="0.0.0.0"):
     try:
         keep_alive.keep_alive(port=port, host=host)
     except Exception:
+        os.system("pip3 install chatterbot==0.8.7 discord.py flask mcstatus pytablemaker python-dotenv pyparsing==2.4.7 numpy packaging psutil")
         print(traceback.format_exc())
         print('Retrying in 5 seconds...')
-        os.system("pip3 install chatterbot==0.8.7 discord.py flask mcstatus pytablemaker python-dotenv numpy pyparsing==2.4.7 numpy packaging")
         time.sleep(5)
         os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
 
     try:
         import bot
-        bot.start(bot=True, reconnect=True)
+        bot.start(os.getenv("DISCORD_TOKEN"), bot=True, reconnect=True)
     except Exception:
         print(traceback.format_exc())
         print('Retrying in 5 seconds...')
