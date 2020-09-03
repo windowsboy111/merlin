c652fe82bedc8316ad0b77380558cae324361c37
windowsboy111 <cyruschan0111@gmail.com>
2020-09-03 17:35:54 +0800

    fixed repl.it compatibility issues

--- src/main.py
+++ src/main.py
@@ -1,25 +1,15 @@
-import keep_alive
+from modules import keep_alive
 import traceback
 import os
 import sys
 import time
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env')
-os.system("pip3 install chatterbot==0.8.7 discord.py flask mcstatus pytablemaker python-dotenv numpy pyparsing==2.4.7 numpy packaging duckduckgo3 && pip3 uninstall ddg -y")
+# os.system("pip3 install chatterbot==0.8.7 discord.py flask mcstatus pytablemaker python-dotenv numpy pyparsing==2.4.7 numpy packaging duckduckgo3 && pip3 uninstall ddg -y")
 
 
 def main(port=8080, host="0.0.0.0"):
-    t = None
-    try:
-        t = keep_alive.keep_alive(port=port, host=host)
-    except Exception:
-        os.system(
-            "pip3 install chatterbot==0.8.7 discord.py flask mcstatus pytablemaker python-dotenv pyparsing==2.4.7 numpy packaging psutil"
-        )
-        print(traceback.format_exc())
-        print('Retrying in 5 seconds...')
-        time.sleep(5)
-        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+    t = keep_alive.keep_alive(port=port, host=host)
     try:
         import bot
         bot.start(os.getenv("DISCORD_TOKEN"), bot=True, reconnect=True)
